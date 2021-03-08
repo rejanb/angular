f966fd251985f46c2beefb5d47110cabb2615f43
rejanb <rejab06@gmail.com>
2021-03-08 09:08:38 +0545

    finals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,68 +9,50 @@ import { jsPDF } from 'jspdf';
 export class AppComponent {
   //code for multiple export
 
-  //  fileArray = new Array<string>();
-  //   // public FileDetails() {
-  //   //   // GET THE FILE INPUT.SS
-  //   //   //var fi = document.getElementById('file');
-  //   //   alert('hello');
-  //   // }
-  //   onFileSelect(input) {
-  //     console.log(input.files);
-  //     if (input.files) {
-  //       for (let i = 0; i < input.files.length; i++) {
-  //         console.log(input.files[i]);
-  //         var reader = new FileReader();
-  //         reader.onload = (e: any) => {
-  //           this.fileArray.push(e.target.result);
-  //           //console.log('Got here: ', e.target.result);
-  //           // this.obj.photoUrl = e.target.result;
-  //         };
-  //         reader.readAsDataURL(input.files[i]);
-  //       }
-  //     }
-  //   }
+  fileArray = new Array<string>();
 
-  //   public download() {
-  //     var doc = new jsPDF('p', 'mm', 'A4'); //jsPDF(orientation, unit, format)
-  //     let header = document.getElementById('header').textContent;
-  //     let firstName = document.getElementById('firstName').textContent;
-  //     let lastName = document.getElementById('lastName').textContent;
-  //     let address = document.getElementById('address').textContent;
-  //     let phone = document.getElementById('phone').textContent;
-  //     let photo = document.getElementById('photo')['src'];
+  onFileSelectss(input) {
+    console.log(input.files);
+    if (input.files) {
+      for (let i = 0; i < input.files.length; i++) {
+        console.log(input.files[i]);
+        var reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.fileArray.push(e.target.result);
+          //console.log('Got here: ', e.target.result);
+          // this.obj.photoUrl = e.target.result;
+        };
+        reader.readAsDataURL(input.files[i]);
+      }
+    }
+  }
 
-  //     //console.log(this.fileArray);
-  //     // fi.onchange = function (event) {
-  //     //   var fileList = fi.files;
-  //     //   //TODO do something with fileList.
-  //     // };
+  public downloadss() {
+    var doc = new jsPDF('p', 'mm', 'A4'); //jsPDF(orientation, unit, format)
+    let header = document.getElementById('header').textContent;
+    let firstName = document.getElementById('firstName').textContent;
+    let lastName = document.getElementById('lastName').textContent;
+    let address = document.getElementById('address').textContent;
+    let phone = document.getElementById('phone').textContent;
+    let photo = document.getElementById('photo')['src'];
 
-  //     //alert(String(fi));
+    doc.setFontSize(20);
+    doc.text(header, 80, 40);
 
-  //     doc.setFontSize(20);
-  //     doc.text(header, 80, 40);
+    doc.addImage(photo, 'JPEG, PNG', 88, 50, 30, 30);
 
-  //     doc.addImage(photo, 'JPEG, PNG', 88, 50, 30, 30);
+    doc.setFontSize(10);
+    doc.text('Full Name  : ' + firstName + '' + lastName, 80, 87);
+    doc.text('Address     : ' + address, 80, 92);
+    doc.text('Phone No  : ' + phone, 80, 97);
 
-  //     doc.setFontSize(10);
-  //     doc.text('Full Name  : ' + firstName + '' + lastName, 80, 87);
-  //     doc.text('Address     : ' + address, 80, 92);
-  //     doc.text('Phone No  : ' + phone, 80, 97);
+    this.fileArray.forEach((file) => {
+      doc.addPage('a4', 'p');
+      doc.addImage(file, 'JPEG, PNG', 0, 0, 300, 300);
+    });
 
-  //     this.fileArray.forEach((file) => {
-  //       doc.addPage('a4', 'p');
-  //       doc.addImage(file, 'JPEG, PNG', 0, 0, 300, 300);
-  //     });
-  //     //doc.addImage(this.fileArray[0], 'JPEG, PNG', 88, 50, 30, 30);
-
-  //     // doc.setFontSize(10);
-  //     // doc.text('Full Name  : ' + firstName + '' + lastName, 80, 87);
-  //     // doc.text('Address     : ' + address, 80, 92);
-  //     // doc.text('Phone No  : ' + phone, 80, 97);
-
-  //     doc.save('convertedPdf.pdf');
-  //   }
+    doc.save('convertedPdf.pdf');
+  }
 
   //code for bulk export
   collection = {};
